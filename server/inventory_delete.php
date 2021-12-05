94aa6747b3310e488430f62554030216a2e274ae
Netipong <netipong42@gmail.com>
2021-12-06 00:14:01 +0700

    test

--- server/inventory_delete.php
+++ server/inventory_delete.php
@@ -2,7 +2,6 @@
 
 require_once("./conn.php");
 
-
 try {
     $data = [
         "id" => $_GET['id']
@@ -10,7 +9,6 @@ try {
     $sql = "DELETE FROM inventory WHERE item_no = :id";
     $query = $conn->prepare($sql);
     $query->execute($data);
-
 
     if ($query) {
         Header("Location:../view/inventory_list.php");
