f7c4db4c9f34073f7bd21445927a803befca7a60
Netipong <netipong42@gmail.com>
2022-01-05 00:13:55 +0700

    donutChart

--- view/main/index.php
+++ view/main/index.php
@@ -44,6 +44,30 @@ $rowChart = json_encode($row);
                 </div>
             </div>
         </div>
+        <div class="col-md-6">
+            <!-- Donut chart -->
+            <div class="card card-primary card-outline">
+                <div class="card-header">
+                    <h3 class="card-title">
+                        <i class="far fa-chart-bar"></i>
+                        Donut Chart
+                    </h3>
+
+                    <div class="card-tools">
+                        <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                            <i class="fas fa-minus"></i>
+                        </button>
+                        <button type="button" class="btn btn-tool" data-card-widget="remove">
+                            <i class="fas fa-times"></i>
+                        </button>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <div id="donut-chart" style="height: 300px;"></div>
+                </div>
+                <!-- /.card-body-->
+            </div>
+        </div>
     </div>
     <!-- BAR CHART -->
     <!-- เนื้อหา -->
@@ -86,7 +110,53 @@ $rowChart = json_encode($row);
                 }
             })
         }
+
+        function donutChart() {
+            var donutData = [{
+                    label: 'Series2',
+                    data: 30,
+                    color: '#3c8dbc'
+                },
+                {
+                    label: 'Series3',
+                    data: 20,
+                    color: '#0073b7'
+                },
+                {
+                    label: 'Series4',
+                    data: 50,
+                    color: '#00c0ef'
+                }
+            ];
+            $.plot('#donut-chart', donutData, {
+                series: {
+                    pie: {
+                        show: true,
+                        radius: 1,
+                        innerRadius: 0.5,
+                        label: {
+                            show: true,
+                            radius: 2 / 3,
+                            formatter: labelFormatter,
+                            threshold: 0.1
+                        }
+
+                    }
+                },
+                legend: {
+                    show: false,
+                }
+            })
+        }
+
+        function labelFormatter(label, series) {
+            return '<div style="font-size:13px; text-align:center; padding:2px; color: #fff; font-weight: 600;">' +
+                label +
+                '<br>' +
+                Math.round(series.percent) + '%</div>'
+        }
         barChart();
+        donutChart();
     </script>
 </body>
 
