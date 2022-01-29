173e0365acded0180a8010bc9bbaa08ad948297a
NTP <netipong42@gmail.com>
2022-01-29 23:42:55 +0700

    :bug: sql

--- view/customers/customers_pdf.php
+++ view/customers/customers_pdf.php
@@ -1,8 +1,6 @@
 <?php
 require_once("../../server/conn.php");
 require_once __DIR__ . '../../../vendor/autoload.php';
-
-
 // PDF
 $defaultConfig = (new Mpdf\Config\ConfigVariables())->getDefaults();
 $fontDirs = $defaultConfig['fontDir'];
